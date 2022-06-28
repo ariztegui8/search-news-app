40c17f70381f20d275a19fc105c113daac698d27
ariztegui8 <jorgeariztegui8@gmail.com>
2022-06-27 23:43:24 -0300

    Primer commit con Buscador de noticias

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Container, Typography, Grid } from "@mui/material";
+import Formulario from "./components/Formulario";
+
+
+function App() {
+  return (
+    <Container>
+      <header>
+        <Typography
+          align='center'
+          marginY={5}
+          component="h1"
+          variant="h3"
+        >
+          Buscador de Noticias
+        </Typography>
+      </header>
+
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} md={6}>
+          <Formulario />
+        </Grid>
+      </Grid>
+
+    </Container>
+  );
+}
+
+export default App;
